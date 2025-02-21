3fa262af6636e0342aa97426fd2f40993db69a2c
Aditya Singh <yeezusgod01@gmail.com>
2025-02-21 08:45:08 +0530

    Added readme.md

---
+++ readme.md
@@ -0,0 +1,1 @@
+Drive video link -> https://drive.google.com/file/d/1YrCZ1hBckwxsnQIxRt6SRI1K1poLuT5K/view?usp=drivesdk
--- speech-to-text-backend/utils/connectToGoogle.js
+++ speech-to-text-backend/utils/connectToGoogle.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { google } from "googleapis";
 
-// const credentials = JSON.parse(fs.readFileSync("./myjson.json"));
+const credentials = JSON.parse(fs.readFileSync("./myjson.json"));
 export const auth = new google.auth.JWT(
     credentials.client_email,
     null,
